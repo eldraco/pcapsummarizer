73075cbcdeae2bffcbaf3b53939b0f61631742e9
eldraco <eldraco@gmail.com>
2021-02-21 19:08:10 +0100

    New metadata creator

---
+++ metadata-processor.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python3
+# Script to manage the creation/update/comsuption of the metadata files for the Stratosphere Datasets
+# Author: Garcia Sebastian, eldraco@gmail.com, sebastian.garcia@agents.fel.cvut.cz
+import argparse
+import sys
+import os
+import time
+from datetime import datetime
+import json
+import subprocess
+
+version = '0.1'
+
+# Files:{
+# capinfos: {
+# Description: from capinfos tool
+# }
+# dnstop: {
+# Description: from dnstop tool
+# }
+# passivedns:  {
+# Description: from passivedns tool
+# }
+# pcap: {
+# Description: original pcap with traffic
+# }
+# bro: {
+# Description: folder with log files of Zeek
+# LastUpdate: 2021/02/20 09:23:23.232323
+# }
+# suricata: {
+# Description: folder with detections of Suricata IDS
+# LastUpdate: 2021/02/20 09:23:23.232323
+# }
+# }
+# Private IP of device: 192.168.1.72      (can be empty)
+# Public IP of device: 147.32.82.234     (can be empty)
+# Operation Comments: {
+# 2021/02/23 11:23:32.342323: We rebooted the device
+# }
+
+
+
+
+
+####################
+# Main
+####################
+if __name__ == '__main__':
+    print('Stratosphere Dataset Metadata Manager. Version {}'.format(version))
+    print('https://stratosphereips.org\n')
+
+    # Parse the parameters
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v', '--verbose', help='Amount of verbosity. This shows more info about the results.', action='store', required=False, type=int)
+    parser.add_argument('-e', '--debug', help='Amount of debugging. This shows inner information about the program.', action='store', required=False, type=int)
+    parser.add_argument('-c', '--createfrom', help='Create a metadata json from a README.md file passed here.', required=False)
+    parser.add_argument('-o', '--output', help='Store the metadata json in this file. Defaults to README.json', type=str, default='README.json', required=False)
+    parser.add_argument('-p', '--pcapfile', help='Pcap file to read. Zeek is run on it and slips interfaces with Zeek.', required=False)
+    parser.add_argument('-r', '--read', help='Read a README.json file and show the data.', required=False)
+    args = parser.parse_args()
+
+    if not args.createfrom and not args.read:
+        print('You need a README.md file to start the conversion, or a README.json to read.')
+        sys.exit()
+
+    if args.createfrom:
+        # Create a new JSON metadata file from a README.md
+
+        metadata = {}
+        metadata['Name'] = 'Stratosphere Dataset Capture'
+        metadata['Description'] = 'Capture Dataset created at the Stratosphere Laboratory, CVUT University.'
+        metadata['Start'] = ''
+        metadata['End'] = ''
+        metadata['Duration'] = ''
+        metadata['Author'] = 'Stratosphere Team'
+        metadata['Contact'] = 'stratosphere@aic.fel.cvut.cz'
+        metadata['Disclaimer'] = 'These files were generated in the Stratosphere Laboratory, AIC group, as part of the Aposemat Project with the support of Avast Software to collect IoT captures at the Czech Technical University, Prague, Czech Republic.'
+        metadata['License'] = 'https://creativecommons.org/licenses/by/3.0/'
+        metadata['URL'] = 'https://mcfp.felk.cvut.cz/publicDatasets/CTU-Malware-Capture-Botnet-150-1'
+        metadata['Citation'] = ''
+        metadata['Files'] = {}
+        metadata['Files']['pcap'] = {'Description': '', 'File':''}
+
+
+        # From README.md file
+        out = subprocess.Popen(["grep", "-i", "Description:", args.createfrom], stdout=subprocess.PIPE)
+        description = ' '.join(out.communicate()[0].decode("utf-8").split()[2:])
+        if description:
+            metadata['Description'] = description
+
+        # Get dataset name if possible
+        out = subprocess.Popen(["grep", "-i", "Generic Dataset name", args.createfrom], stdout=subprocess.PIPE)
+        name = ' '.join(out.communicate()[0].decode("utf-8").split()[4:])
+        if name:
+            metadata['Name'] = name
+
+        # If it was malware, get malware name
+        out = subprocess.Popen(["grep", "-i", "Probable Malware Name", args.createfrom], stdout=subprocess.PIPE)
+        malwarename = ' '.join(out.communicate()[0].decode("utf-8").split()[4:])
+        if malwarename:
+            metadata['Malware Name'] = malwarename
+
+
+        #"SHA256"
+
+        #"device:"
+
+        #"Suricata run"
+
+        #"Password"
+
+        # From capinfo file
+        capinfosfile = os.path.isfile(os.path.join('.', '*'))
+        starttime = ''
+        endtime = ''
+        metadata['Citation'] = 'Stratosphere Laboratory Dataset ' + metadata['Name'] + '. ' + metadata['Start'] + '. Stratosphere Team.'
+
+        # Store metadata in a file
+        print(metadata)
+        outputfile = open(args.output, 'w')
+        metadatajson = json.dump(metadata, outputfile)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
